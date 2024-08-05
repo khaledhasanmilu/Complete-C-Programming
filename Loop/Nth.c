2016e934bc68626a6feafe52d7aac23280260804
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2024-08-05 23:27:57 +0600

    Add files via upload

---
+++ Loop/Nth.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+  int n;
+  scanf("%d",&n);
+  for (int i = 1; i <= n; i++)
+  {
+    printf("%d",i);
+    if (i!=n)
+    {
+      printf(", ");
+    }
+    
+  }
+  printf("\n");
+  
+  return 0;
+}
