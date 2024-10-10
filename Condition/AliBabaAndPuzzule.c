ca7d1cd2dd890b17ba1c3136096d75a8cc38413e
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2024-10-10 21:30:04 +0600

    Add files via upload

---
+++ Condition/AliBabaAndPuzzule.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c, d;
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+    if (a - b + c == d || a + b - c == d || a + b * c == d || a * b + c == d || a - b * c == d || a * b - c == d)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    return 0;
+}
