843c34b180b998818f387710939e8ec4104a3101
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2024-10-11 21:57:53 +0600

    Add files via upload

---
+++ Loop/patternulta.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for (int i = n; i >=1; i--)
+    {
+        for (int j = i; j >=1 ; j--)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
